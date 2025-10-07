41d63c348acd794b848d281b668e2a1b393cbfae
Dedede12345 <gleb@adenisov.com>
2025-10-07 14:53:48 +0200

    Modified forms

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 
-def post_list(request):
-    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+from .forms import EmailPostForm
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -26,5 +25,29 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
     )
 
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,8 +8,19 @@
 #python3 manage.py flush --no-input
 python manage.py migrate
 
+# Collect static files
+STATIC_DIR="/usr/src/app/staticfiles"
+
+if [ -d "$STATIC_DIR" ] && [ "$(ls -A $STATIC_DIR)" ]; then
+    echo "[*] Static files present"
+else
+    echo "[*] Collecting static files"
+    python manage.py collectstatic --noinput
+fi
+
 
 # Create superuser if it doesn't exist
+echo "[*] Creating superuser"
 DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-admin}
 DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-admin@example.com}
 DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-adminpass}
